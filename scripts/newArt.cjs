28d0b95403c5b4e02e0ee28f75c18f1e55a52e8c
delta <deltronik98@gmail.com>
2025-06-09 16:55:08 +0000

    feat[package.json]:creación de un script para automatizar la creación de contenido del blog - newArt.cjs: crea una carpeta en 'content/blog/<newFolder>/ - newArt.cjs: crea un archivo en la carpeta creada <newFolder>/index.md - yarn newart: comando que hace la llamada al script

---
+++ scripts/newArt.cjs
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const path = require("path");
+
+const slug = process.argv[2];
+
+if (!slug) {
+  console.error("Proporcina un nombre para el Post ");
+  process.exit(1);
+}
+
+const title = slug
+  .split("-")
+  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(" ");
+
+const date = new Date().toISOString().split("T")[0];
+
+// Ruta del nuevo folder
+const folderPath = path.join(__dirname, "../src/content/blog", slug);
+
+// Ruta del archivo Markdown dentro del folder
+const filePath = path.join(folderPath, "index.md");
+
+const content = `---
+title: "${title}"
+description: "Descripción del post"
+date: "${date}"
+draft: true
+tags: ["", ""]
+categories: [""]
+---
+
+# ${title}
+
+`;
+
+if (fs.existsSync(folderPath)) {
+  console.error("❌ Ya existe una carpeta con ese nombre. Elige otro nombre.");
+  process.exit(1);
+}
+
+// Crear carpeta
+fs.mkdirSync(folderPath, { recursive: true });
+
+// Crear archivo Markdown
+fs.writeFileSync(filePath, content);
+
+console.log(`✅ Carpeta y archivo creados en: ${folderPath}`);
---
+++ src/content/blog/hello-word/index.md
@@ -0,0 +1,11 @@
+---
+title: "Hello Word"
+description: "Descripción del post"
+date: "2025-06-09"
+draft: true
+tags: ["tag","intro","example"]
+---
+
+# Hello Word
+
+Esto es un ejemplo de texto largo, parrafo de ejemplo.
